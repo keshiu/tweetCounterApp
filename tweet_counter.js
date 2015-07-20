5ba00dd272789c3fd4c4da4fbb99971d46e1afe2
Alena Arsenyeva <keshiu@list.ru>
2015-07-21 01:59:31 +0300

    Add saving wordcounts in Redis

--- tweet_counter.js
+++ tweet_counter.js
@@ -1,37 +1,47 @@
 var setUpTweetCounter = function(words) {
-	var ntwitter = require("ntwitter"),
-	credentials = require("./credentials.json"),
-	twitter,
-	counts = {};
-	console.log("words are: " + words);
-	words.forEach(function(word) {
-		counts[word] = 0;
-		console.log("counts[" + word + "]" + counts[word]);
-	});
+    var ntwitter = require("ntwitter"),
+    redis = require("redis"),
+    credentials = require("./credentials.json"),
+    twitter,
+    redisClient,
+    counts = {};
 	
-	twitter = ntwitter(credentials);
-	twitter.stream(
-		"statuses/filter",
-		{track: words},
-		function(stream) {
-			stream.on("data", function(tweet) {
-				words.forEach(function(word) {
-					if(tweet.text.indexOf(word) > -1) {
-						counts[word] = counts[word] + 1;
-					}
-				});		
-			});
-			stream.on('error', function(error, code) {
-    				console.log("My error: " + error + ": " + code);
-			});
-		}
-	);
+    twitter = ntwitter(credentials);
+    redisClient = redis.createClient();
+    redisClient.mget(words, function(err, results) {
+        if (err !== null) {
+    	    console.log("ERROR: " + err);
+    	    return;
+   	    }
+   	    var i = 0;
+   	    words.forEach(function(word) {
+    	    counts[word] = parseInt(results[i],10) || 0;
+    	    i++;
+    	});
+    });
+    twitter.stream(
+        "statuses/filter",
+        {track: words},
+        function(stream) {
+            stream.on("data", function(tweet) {
+                words.forEach(function(word) {
+                    if(tweet.text.indexOf(word) > -1) {
+                    	redisClient.incr(word);
+                        counts[word] = counts[word] + 1;
+                    }
+                });		
+            });
+            stream.on('error', function(error, code) {
+                console.log("My error: " + error + ": " + code);
+            });
+        }
+    );
 
-	setInterval(function() {
-		for (var i = 0; i < words.length; i++) {
-			console.log(words[i] + ": " + counts[words[i]]);
-		};
-	}, 3000);
-	return counts;
+    setInterval(function() {
+        for (var i = 0; i < words.length; i++) {
+            console.log(words[i] + ": " + counts[words[i]]);
+        };
+    }, 3000);
+    return counts;
 }
 module.exports = setUpTweetCounter;
